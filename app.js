e2fb4a222e2175181ae95c4b15c029e7c395532a
Leon Boot <wlboot@gmail.com>
2017-06-06 19:44:29 +0200

    Removed graphite, output to JSON, port CLI argument

--- app.js
+++ app.js
@@ -1,50 +1,15 @@
+var argv = require('minimist')(process.argv.slice(2));
+var serialPort = argv.hasOwnProperty('port') ? argv.port : '/dev/TTYUSB0';
+
 var P1Reader = require('p1-reader');
-var reader = new P1Reader({serialPort: '/dev/ttyUSB0', debug: false});
-
-var graphite = require('graphite');
-var client = graphite.createClient('plaintext://graphite.cloud.nw153.nl:2003/');
-
-const httpCodes = {
-  200: "OK",
-	201: "Created",
-	202: "Accepted",
-	304: "Not Modified",
-	400: "Bad Request",
-	401: "Unauthorized",
-	403: "Forbidden",
-	404: "Not Found",
-	405: "Resource Not Allowed",
-	406: "Not Acceptable",
-	409: "Conflict",
-	412: "Precondition Failed",
-	415: "Bad Content Type",
-	416: "Requested Range Not Satisfiable",
-	417: "Expectation Failed",
-	500: "Internal Server Error"
-}
+var reader = new P1Reader({serialPort: serialPort, debug: false});
 
 reader.on('reading', function (data) {
-	const toSave = {
-		equipmentId: data.equipmentId,
-		timestamp: new Date(data.timestamp),
-		electricity: {
-			received: {
-				tariff1: data.electricity.received.tariff1.reading,
-				tariff2: data.electricity.received.tariff2.reading,
-				actual: data.electricity.received.actual.reading
-			},
-			tariffIndicator: data.electricity.tariffIndicator
-		},
-		gas: {
-			timestamp: new Date(data.gas.timestamp),
-			usage: data.gas.reading
-		}
-	};
-	console.log('home.utility.electricity.usage ' + (data.electricity.received.actual.reading * 1000).toString() + ' ' + ((new Date(data.timestamp)).getTime() / 1000).toString());
-	client.write({"home.utility.electricity.usage": data.electricity.received.actual.reading * 1000}, (new Date(data.timestamp)).getTime() / 1000);
-        client.write({"home.utility.gas.usage": data.gas.reading}, (new Date(data.gas.timestamp)).getTime() / 1000);
+	process.stdout.write(JSON.stringify(data) + '\n');
+	process.exit(0);
 });
 
 reader.on('error', function (err) {
-	console.log('Error: ', err);
+	console.error(err);
+	process.exit(1);
 });
